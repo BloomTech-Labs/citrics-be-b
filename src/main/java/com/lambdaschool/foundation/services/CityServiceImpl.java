0b588ead1df2d677b3761352874157da69c6fa1f
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-10-13 15:07:29 -0400

    added default root endopoint and endpoint to return city with average fields

--- src/main/java/com/lambdaschool/foundation/services/CityServiceImpl.java
+++ src/main/java/com/lambdaschool/foundation/services/CityServiceImpl.java
@@ -24,6 +24,7 @@ public class CityServiceImpl implements CityService
 
     /**
      * Find all cities in DB
+     *
      * @return list of Cities
      */
     @Override
@@ -40,6 +41,7 @@ public class CityServiceImpl implements CityService
 
     /**
      * find city by cityid
+     *
      * @param id local id of city
      * @return City object matching the city id or
      * @throws ResourceNotFoundException
@@ -53,6 +55,7 @@ public class CityServiceImpl implements CityService
 
     /**
      * Saves new city to DB
+     *
      * @param city new city to be saved
      * @return newly saved city
      */
@@ -62,9 +65,10 @@ public class CityServiceImpl implements CityService
     {
         City newCity = new City();
 
-        if(city.getCityid() != 0)
+        if (city.getCityid() != 0)
         {
-            cityrepo.findById(city.getCityid()).orElseThrow(() -> new ResourceNotFoundException("City id " + city.getCityid() + " not found!"));
+            cityrepo.findById(city.getCityid())
+                .orElseThrow(() -> new ResourceNotFoundException("City id " + city.getCityid() + " not found!"));
             newCity.setCityid(city.getCityid());
         }
 
@@ -82,6 +86,7 @@ public class CityServiceImpl implements CityService
 
     /**
      * Saves new city from DS API schema
+     *
      * @param city JSON City to be saved
      * @return newly saved City object
      */
@@ -104,6 +109,7 @@ public class CityServiceImpl implements CityService
 
     /**
      * Find city by citynamestate
+     *
      * @param name citynamestate
      * @return city object match name or throws exception
      */
@@ -118,13 +124,89 @@ public class CityServiceImpl implements CityService
         return c;
     }
 
+    /**
+     * Find all cities and their name's + id's
+     * @return List of City name's and Id's
+     */
     @Override
     public List<CityIdName> findAllIds()
     {
         List<CityIdName> cities = new ArrayList<>();
 
-        cityrepo.findAll().iterator().forEachRemaining((city) -> cities.add(new CityIdName(city.getCityid(), city.getCitynamestate())));
+        cityrepo.findAll()
+            .iterator()
+            .forEachRemaining((city) -> cities.add(new CityIdName(city.getCityid(),
+                city.getCitynamestate())));
 
         return cities;
+    }
+
+    /**
+     * Find the average value for all city fields
+     * @return a City with the field averages of all cities
+     */
+    @Override
+    public City findAverageCity()
+    {
+        List<City> cities = new ArrayList<>();
+
+        City c = new City();
+        int totalCities = 0;
+        String cityNameState = "National Average, USA";
+        double totalPopulation = 0;
+        int totalPopulationDensityRating = 0;
+        int totalSafteyRatingScore = 0;
+        int totalCostofLivingScore = 0;
+        double totalAverageIncome = 0;
+        int totalAverageTemperature = 0;
+        float totalLat = 0;
+        float totalLon = 0;
+        double totalAverageAge = 0;
+        double totalAverageHouseholdIncome = 0;
+        double totalAverageIndividualIncome = 0;
+        double totalAverageHouseingCost = 0;
+        double totalAverageRentCost = 0;
+        double totalCostOfLivingIndex = 0;
+
+        cityrepo.findAll()
+            .iterator()
+            .forEachRemaining(cities::add);
+
+        for (int i = 0; i < cities.size(); i++)
+        {
+            totalCities++;
+            totalPopulation += cities.get(i).getPopulation();
+            totalPopulationDensityRating += cities.get(i).getPopulationdensityrating();
+            totalSafteyRatingScore += cities.get(i).getSafteyratingscore();
+            totalCostofLivingScore += cities.get(i).getCostoflivingscore();
+            totalAverageIncome += cities.get(i).getAverageincome();
+            totalAverageTemperature += cities.get(i).getAveragetemperature();
+            totalLat += cities.get(i).getLat();
+            totalLon += cities.get(i).getLon();
+            totalAverageAge += cities.get(i).getAverageage();
+            totalAverageHouseholdIncome += cities.get(i).getAveragehouseholdincome();
+            totalAverageIndividualIncome += cities.get(i).getAverageindividualincome();
+            totalAverageHouseingCost += cities.get(i).getAveragehouseingcost();
+            totalAverageRentCost += cities.get(i).getAveragerentcost();
+            totalCostOfLivingIndex += cities.get(i).getCostoflivingindex();
+        }
+
+        c.setCitynamestate(cityNameState);
+        c.setPopulation(totalPopulation / totalCities);
+        c.setPopulationdensityrating(totalPopulationDensityRating / totalCities);
+        c.setSafteyratingscore(totalSafteyRatingScore / totalCities);
+        c.setCostoflivingscore(totalCostofLivingScore / totalCities);
+        c.setAverageincome(totalAverageIncome / totalCities);
+        c.setAveragetemperature(totalAverageTemperature / totalCities);
+        c.setLat(totalLat / totalCities);
+        c.setLon(totalLon / totalCities);
+        c.setAverageage(totalAverageAge / totalCities);
+        c.setAveragehouseholdincome(totalAverageHouseholdIncome / totalCities);
+        c.setAverageindividualincome(totalAverageIndividualIncome / totalCities);
+        c.setAveragehouseingcost(totalAverageHouseingCost / totalCities);
+        c.setAveragerentcost(totalAverageRentCost / totalCities);
+        c.setCostoflivingindex(totalCostOfLivingIndex / totalCities);
+
+        return c;
     }
 }
