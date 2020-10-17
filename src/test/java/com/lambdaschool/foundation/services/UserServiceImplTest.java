658f788cb7d68dd8f50562dc6f89281c639fb9fb
Shane Kaestner <shane.kaestner@yahoo.com>
2020-10-17 16:09:38 -0700

    added two passing tests for user service

--- src/test/java/com/lambdaschool/foundation/services/UserServiceImplTest.java
+++ src/test/java/com/lambdaschool/foundation/services/UserServiceImplTest.java
@@ -1,63 +1,96 @@
 package com.lambdaschool.foundation.services;
 
+import com.lambdaschool.foundation.models.User;
+import com.lambdaschool.foundation.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
-class UserServiceImplTest
-{
+class UserServiceImplTest {
+
+    @Autowired
+    UserService userService;
+
+    @MockBean
+    private UserRepository userrepos;
 
     @BeforeEach
-    void setUp()
-    {
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @AfterEach
-    void tearDown()
-    {
+    void tearDown() {
     }
 
     @Test
-    void findUserById()
-    {
+    void findUserById() {
+        User u1 = new User("Arthur");
+        u1.setUserid(1);
+
+        Mockito.doReturn(Optional.of(u1))
+            .when(userrepos)
+            .findById(1L);
+
+        u1 = userService.findUserById(1);
+
+        // will return lowercase name.
+        assertEquals("arthur",
+            u1.getUsername());
     }
 
     @Test
-    void findByNameContaining()
-    {
+    void findByNameContaining() {
+        User u1 = new User("Arthur");
+        User u2 = new User("Cathcart");
+
+        Mockito.doReturn(Arrays.asList(u1, u2))
+            .when(userrepos)
+            .findByUsernameContainingIgnoreCase("art");
+
+        // intentional capital R to test ignoring case functionality
+        List<User> users = userService.findByNameContaining("aRt");
+
+        // will return lowercase name
+        assertEquals(2,
+            userService.findByNameContaining("art")
+                .size());
     }
 
     @Test
-    void findAll()
-    {
+    void findAll() {
     }
 
     @Test
-    void delete()
-    {
+    void delete() {
     }
 
     @Test
-    void findByName()
-    {
+    void findByName() {
     }
 
     @Test
-    void save()
-    {
+    void save() {
     }
 
     @Test
-    void update()
-    {
+    void update() {
     }
 
     @Test
-    void deleteAll()
-    {
+    void deleteAll() {
     }
 }
