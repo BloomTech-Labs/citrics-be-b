ed962872a2df25de5f6ab6666c1930a20e1585f6
Harry Henry Gebel <harry@gebel.tech>
2020-11-11 18:29:04 -0500

    Scrape cities table

--- utilities/scraper.py
+++ utilities/scraper.py
@@ -8,6 +8,7 @@ for city in cities_data:
     cities.append(
         get('https://labs27-c-citrics-api.herokuapp.com/cities/city/%d' %
             city['cityid']).json())
+    print(len(cities))
 
 file = open('data.sql', 'w')
 file.write(
@@ -18,16 +19,25 @@ file.write(
     ' WEBSITE)\n' +
     'VALUES')
 for i in range(1, len(cities) + 1):
+    print(i)
     city = cities[i - 1]
     city_name = city['citynamestate'].split(',')[0]
-    if i > 1:
-        file.write(',\n')
-    file.write('(%d, CURRENT_USER, CURRENT_TIMESTAMP, CURRENT_USER, CURRENT_TIMESTAMP,\n' % i)
-    file.write("'%s', %f, %f,\n'%s', %f, '%s',\n%f, %f, %f, %f, %f,\n%f, '%s', '%s', '%s')" %
-               (city['acastatus'], city['averageage'], city['averagehouse'],
-      city_name, city['costoflivingindex'], city['fpis'],
-      city['householdincome'], city['latitude'], city['logitude'], city['population'], city['densitykmsq'],
-      city['rent'], city['statecode'], city['timezone'], city['wikiimgurl']))
+    try:
+        url = city['wikiimgurl'][:254]
+    except:
+        url = ""
+    insert_data =(city['acastatus'], city['averageage'], city['averagehouse'],
+                  city_name, city['costoflivingindex'], city['fpis'],
+                  city['householdincome'], city['latitude'], city['logitude'], city['population'], city['densitymisq'] / 2.58999,
+                  city['rent'], city['statecode'], city['timezone'], url)
+    try:
+        insert = "'%s', %f, %f,\n'%s', %f, '%s',\n%f, %f, %f, %f, %f,\n%f, '%s', '%s', '%s')" % insert_data
+        if i > 1:
+            file.write(',\n')
+        file.write('(%d, CURRENT_USER, CURRENT_TIMESTAMP, CURRENT_USER, CURRENT_TIMESTAMP,\n' % i)
+        file.write(insert);
+    except:
+        print(insert_data)
 file.write(';\n')
 
 file.close()
