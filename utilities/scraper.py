677dce3c5ad3e8d3c663b72bf7f0b7195c8ce676
Harry Henry Gebel <harry@gebel.tech>
2020-11-12 18:24:56 -0500

    Convert to json

--- utilities/scraper.py
+++ utilities/scraper.py
@@ -1,14 +1,7 @@
-from requests import get
+from json import load
 
-cities_data = get(
-    'https://labs27-c-citrics-api.herokuapp.com/cities/allid').json()
-
-cities = []
-for city in cities_data:
-    cities.append(
-        get('https://labs27-c-citrics-api.herokuapp.com/cities/city/%d' %
-            city['cityid']).json())
-    print(len(cities))
+with open('database.json', 'r') as file:
+    cities = load(file)
 
 file = open('data.sql', 'w')
 file.write(
@@ -22,22 +15,16 @@ for i in range(1, len(cities) + 1):
     print(i)
     city = cities[i - 1]
     city_name = city['citynamestate'].split(',')[0]
-    try:
-        url = city['wikiimgurl'][:254]
-    except:
-        url = ""
+    url = city['wikiimgurl'][:254]
     insert_data =(city['acastatus'], city['averageage'], city['averagehouse'],
                   city_name, city['costoflivingindex'], city['fpis'],
                   city['householdincome'], city['latitude'], city['logitude'], city['population'], city['densitymisq'] / 2.58999,
                   city['rent'], city['statecode'], city['timezone'], url)
-    try:
-        insert = "'%s', %f, %f,\n'%s', %f, '%s',\n%f, %f, %f, %f, %f,\n%f, '%s', '%s', '%s')" % insert_data
-        if i > 1:
-            file.write(',\n')
-        file.write('(%d, CURRENT_USER, CURRENT_TIMESTAMP, CURRENT_USER, CURRENT_TIMESTAMP,\n' % i)
-        file.write(insert);
-    except:
-        print(insert_data)
+    insert = "'%s', %f, %f,\n'%s', %f, '%s',\n%f, %f, %f, %f, %f,\n%f, '%s', '%s', '%s')" % insert_data
+    if i > 1:
+        file.write(',\n')
+    file.write('(%d, CURRENT_USER, CURRENT_TIMESTAMP, CURRENT_USER, CURRENT_TIMESTAMP,\n' % i)
+    file.write(insert);
 file.write(';\n')
 
 file.close()
