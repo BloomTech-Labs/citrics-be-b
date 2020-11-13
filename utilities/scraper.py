19c80f81f70356bbc9e2d07bc4e896dd853276d1
Harry Henry Gebel <harry@gebel.tech>
2020-11-12 19:56:56 -0500

    Add historical housing data

--- utilities/scraper.py
+++ utilities/scraper.py
@@ -3,6 +3,9 @@ from json import load
 with open('database.json', 'r') as file:
     cities = load(file)
 
+historical_housing = []
+
+# cities table
 file = open('data.sql', 'w')
 file.write(
     'INSERT INTO CITIES (CITY_ID, CREATED_BY, CREATED_DATE,  LAST_MODIFIED_BY,\n' +
@@ -23,7 +26,29 @@ for i in range(1, len(cities) + 1):
     insert = "'%s', %f, %f,\n'%s', %f, '%s',\n%f, %f, %f, %f, %f,\n%f, '%s', '%s', '%s')" % insert_data
     if i > 1:
         file.write(',\n')
-    file.write('(%d, CURRENT_USER, CURRENT_TIMESTAMP, CURRENT_USER, CURRENT_TIMESTAMP,\n' % i)
+    file.write('(%i, CURRENT_USER, CURRENT_TIMESTAMP, CURRENT_USER, CURRENT_TIMESTAMP,\n' % i)
+    file.write(insert);
+
+    # historical house prices store
+    months = city['historicalaveragehouse']
+    for month in months:
+        month['city_id'] = i
+    historical_housing.extend(months)
+file.write(';\n')
+
+# historical house prices
+file.write(
+    'INSERT INTO historical_home_cost (house_id, CREATED_BY, CREATED_DATE,\n'
+    'LAST_MODIFIED_BY, LAST_MODIFIED_DATE, city_id, year, month, home_cost)\n'
+    'VALUES')
+for i in range(1, len(historical_housing) + 1):
+    housing = historical_housing[i - 1]
+    insert_data =(housing['city_id'], housing['year'], housing['month'],
+                  housing['housingcost'])
+    insert = "%i, %i, %i, %f)" % insert_data
+    if i > 1:
+        file.write(',\n')
+    file.write('(%i, CURRENT_USER, CURRENT_TIMESTAMP, CURRENT_USER, CURRENT_TIMESTAMP,\n' % i)
     file.write(insert);
 file.write(';\n')
 
