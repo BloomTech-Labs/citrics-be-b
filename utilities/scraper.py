12181423d76c0763da30a5cd4973b1bb8b9cfce1
Harry Henry Gebel <harry@gebel.tech>
2020-11-13 10:55:15 -0500

    Put image url in correct field

--- utilities/scraper.py
+++ utilities/scraper.py
@@ -21,7 +21,7 @@ file.write(
     ' LAST_MODIFIED_DATE, ACA_STATUS, AVERAGE_AGE, AVERAGE_HOME_COST,\n' +
     ' CITY_NAME, COST_OF_LIVING_INDEX, FPIS, HOUSEHOLD_INCOME, LATITUDE,\n' +
     ' LONGITUDE, POPULATION, POPULATION_DENSITY, RENT, STATE_CODE, TIMEZONE,\n' +
-    ' WEBSITE)\n' +
+    ' IMAGE_URL, WEBSITE)\n' +
     'VALUES')
 for i in range(1, len(cities) + 1):
     city = cities[i - 1]
@@ -31,8 +31,9 @@ for i in range(1, len(cities) + 1):
                    city_name, city['costoflivingindex'], city['fpis'],
                    city['householdincome'], city['latitude'], city['logitude'],
                    city['population'], city['densitymisq'] / 2.58999,
-                   city['rent'], city['statecode'], city['timezone'], url)
-    insert = "'%s', %f, %f,\n'%s', %f, '%s',\n%f, %f, %f, %f, %f,\n%f, '%s', '%s', '%s')" % insert_data
+                   city['rent'], city['statecode'], city['timezone'], url,
+                   city['website'])
+    insert = "'%s', %f, %f,\n'%s', %f, '%s',\n%f, %f, %f, %f, %f,\n%f, '%s', '%s', '%s', '%s')" % insert_data
     if i > 1:
         file.write(',\n')
     file.write('(%i, CURRENT_USER, CURRENT_TIMESTAMP, CURRENT_USER, CURRENT_TIMESTAMP,\n' % i)
