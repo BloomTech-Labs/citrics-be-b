67ab6290acc77715ae49148c1a713e0c3ba4d49c
Harry Henry Gebel <harry@gebel.tech>
2020-11-17 12:39:51 -0500

    Cleanup

--- src/main/java/com/lambdaschool/foundation/services/CityService.java
+++ src/main/java/com/lambdaschool/foundation/services/CityService.java
@@ -2,8 +2,8 @@ package com.lambdaschool.foundation.services;
 
 import com.lambdaschool.foundation.models.City;
 import com.lambdaschool.foundation.models.CityAbstract;
-import com.lambdaschool.foundation.models.DSCity;
 import com.lambdaschool.foundation.models.User;
+
 import java.util.List;
 
 public interface CityService {
@@ -14,8 +14,8 @@ public interface CityService {
   List<City> findAll();
 
   /**
-   * Finds city by cityid
-   * @param id cityid
+   * Finds city by city id
+   * @param id city id
    * @return City matching id
    */
   City findCityById(long id);
@@ -41,12 +41,12 @@ public interface CityService {
    */
   City save(City city);
 
-  /**
-   * Saves new city from DS API schema
-   * @param city new city to be saved
-   * @return newly saved city
-   */
-  City saveDs(DSCity city) throws Exception;
+//  /**
+//   * Saves new city from DS API schema
+//   * @param city new city to be saved
+//   * @return newly saved city
+//   */
+//  City saveDs(DSCity city) throws Exception;
 
   /**
    * Finds city by cityName field
@@ -61,21 +61,21 @@ public interface CityService {
    */
   List<CityAbstract> findAllIds();
 
-  /**
-   * Finds the average value for all fields in cities excluding the historical data
-   * @return a City object with the average of all city fields in the DB
-   */
-  City findAverageCity();
-
-  /**
-   * Finds the average city object stored in DB
-   * @return
-   */
-  City returnAverageCity();
+//  /**
+//   * Finds the average value for all fields in cities excluding the historical data
+//   * @return a City object with the average of all city fields in the DB
+//   */
+//  City findAverageCity();
+//
+//  /**
+//   * Finds the average city object stored in DB
+//   * @return
+//   */
+//  City returnAverageCity();
 
   /**
    * Saves city to current users fav cities list
-   * @param id cityid of city to be saved
+   * @param id city id of city to be saved
    * @param user user extracted by controller
    */
   void saveFavCity(long id, User user);
